5ee265a162e0a3886e4694b1a5e8c40974607978
HuseyinUnalan <116941857+HuseyinUnalan@users.noreply.github.com>
2022-11-23 11:02:54 +0300

    Add files via upload

---
+++ Odev3/menu.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-expand-sm bg-light justify-content-center">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Kayıt Formu</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="liste.php">Liste</a>
+    </li>
+  </ul>
+</nav>
